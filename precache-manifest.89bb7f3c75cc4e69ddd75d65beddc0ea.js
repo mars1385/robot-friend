26cece64143ee11870045bf9b747797b04198b64
mars1385 <you@example.com>
2019-03-09 21:14:22 +0330

    Updates

---
+++ precache-manifest.89bb7f3c75cc4e69ddd75d65beddc0ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-friend/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d74fbbf5cadf326ac023",
+    "url": "/robot-friend/static/js/runtime~main.1efb1a82.js"
+  },
+  {
+    "revision": "b9b6efedc9e349c5e39d",
+    "url": "/robot-friend/static/js/main.1d677566.chunk.js"
+  },
+  {
+    "revision": "c4a9f83ad5c883527cb7",
+    "url": "/robot-friend/static/js/2.72924a2b.chunk.js"
+  },
+  {
+    "revision": "b9b6efedc9e349c5e39d",
+    "url": "/robot-friend/static/css/main.4eefe505.chunk.css"
+  },
+  {
+    "revision": "c4a9f83ad5c883527cb7",
+    "url": "/robot-friend/static/css/2.39350280.chunk.css"
+  },
+  {
+    "revision": "984c92bf2dfa1681ace6362be682a5ce",
+    "url": "/robot-friend/index.html"
+  }
+];
